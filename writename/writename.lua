0000000ad43b743d50eb9804176a19768be4c79b
Jeff Sandberg <paradox460@gmail.com>
2021-08-16 01:41:37 -0700

    Fix boolean arg parsing in writename.lua
    Not a perfect fix, but should cover all config file variations.
    Now, to set a param to the script-message-to command, it _must_ be a
    `true` to be true, all other strings are falsey.

--- writename/writename.lua
+++ writename/writename.lua
@@ -1,5 +1,9 @@
 require "mp.msg"
 
+function checkBool(checkVal)
+  return (checkVal and checkVal == "true")
+end
+
 function unmute()
   mp.unregister_event(unmute)
   mp.set_property_bool("mute", false)
@@ -9,14 +13,14 @@ function write_name(filename, skip, mute)
   file = io.open(filename, "a+")
   path = mp.get_property("path")
   file:write(mp.get_property("path"), "\n")
-  if skip then
+  if checkBool(skip) then
     mp.command("playlist-remove current")
   end
   mp.osd_message("Wrote to " .. filename)
   mp.msg.info("Wrote " .. path .. " to " .. filename)
   file:flush()
   file:close()
-  if mute then
+  if checkBool(mute) then
     mp.set_property_bool("mute", true)
     mp.register_event("end-file", unmute)
   end
